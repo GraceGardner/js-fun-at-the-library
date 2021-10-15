1c40398558dd613941cc759a8ac3d389bf71a7a5
Grace Gardner <graceirisgardner@gmail.com>
2021-10-14 20:35:32 -0600

    complete warm up

--- src/warm-up.js
+++ src/warm-up.js
@@ -9,30 +9,37 @@
 
 // Initialize two new variables, "title", and "pages", and assign values to them with
 // a book's title and page count.
-
+var title = "Storm Front";
+var pages = 322;
 // var title = "Harry Potter";
 // var ...
-
+console.log(title);
+console.log(pages);
 
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
-// of what it currently is 
+// of what it currently is
+pages = pages / 4;
+console.log(pages);
 
 
 
 // Initialize a new variable "movie", but leave it undefined
-
+var movie = undefined;
+console.log(movie);
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
-
+var sequel = "Fool Moon";
+console.log(sequel);
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
-
+var isFavorite = true;
+console.log(isFavorite);
 
 
 
@@ -40,15 +47,38 @@
 // assign a boolean value to this variable. If "pages" is less than 25, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
+var isShortStory = checkShortStory(pages);
 
+function checkShortStory(pages) {
+  if(pages < 25) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+  console.log(isShortStory);
+  pages = 10;
+  console.log(isShortStory);
 
 
 //------------------------- Conditionals -------------------------//
 
 
 // Express the following in code: If the variable "pages" is less than 1000 log
-// the statement 'I could read that in a day!', otherwise, log the statement 
+// the statement 'I could read that in a day!', otherwise, log the statement
 // 'Ok, maybe I could read that in a day and a half'
+function determineReadSpeed(pages){
+  if (pages < 1000){
+    return "I could read that in a day!";
+  } else if (pages >= 1000){
+    return "Ok, maybe I could read that in a day and a half"
+  }
+}
+
+console.log(determineReadSpeed(pages));
+pages = 5000;
+console.log(determineReadSpeed(pages));
 
 
 
@@ -56,33 +86,53 @@
 // log the statement 'My favorite book is [title]!'. Otherwise, log the
 // statement 'I recently read the [title] book'. In either case, the value stored in
 // the "title" variable should be included in the logged statement.
+function checkBook(title, isFavorite){
+  if (isFavorite === true) {
+    return `My favorite book is ${title}`;
+  } else {
+    return `I recently read the ${title} book`;
+  }
+}
 
-
+console.log(checkBook(title, isFavorite));
+isFavorite = false;
+console.log(checkBook(title, isFavorite));
 
 //------------------------- Arrays -------------------------//
 
-
+var books = ["Green Eggs and Ham", "Harry Potter", "Dracula", "Dune", "Of Mice and Men"];
+console.log(books);
 // Create a new variable "books" and assign it to an Array of 5 different
 // book titles, represented by their names as Strings.
 
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
+console.log(books[2]);
 
 
 // Create a new variable "belowTwoHundred" and assign it to an Array of 12
 // different numbers below two hundred.
-
+var belowTwoHundred = [101, 99, 89, 43, 54, 65, 23, 190, 70, 33, 23, 42];
+console.log(belowTwoHundred);
+console.log(belowTwoHundred[4]);
 
 // Write the code to access the 5th element of the array, using bracket notation.
 
 
 //------------------------- Objects -------------------------//
 
+var dictionary = {
+  name: "Webster's",
+  hasThesaurus: false
+}
+console.log(dictionary)
+
 // Create a new variable "dictionary" and assign to it an object literal with two
 // keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
 // the value for hasThesaurus should be false
-
+dictionary.pageCount = 720;
+console.log(dictionary);
 
 // Write the code to add the key of pageCount, with the value of 720. Use dot
 // notation to do this. DO NOT modify your code from above.
