004035addcae1724fb0e1e151334e7071adae1e9
Grace Gardner <graceirisgardner@gmail.com>
2021-10-14 21:30:04 -0600

    Complete book tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(review, reviews){
+
+   if (!reviews.includes(review)) {
+     reviews.push(review);
+   }
+ return reviews;
+}
+
+function calculatePageCount(bookTitle) {
+  return bookTitle.length * 20;
+}
+
+function writeBook(bookTitle, bookCharacter, genre) {
+  return{
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    genre: genre,
+    pageCount: calculatePageCount(bookTitle)
+  }
+}
+
+function editBook(book) {
+  return book.pageCount = (book.pageCount / 4) * 3;
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
