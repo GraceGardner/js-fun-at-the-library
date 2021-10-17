da7fca4845fa7c9317ac277d6466474bd2f33bdf
Grace Gardner <graceirisgardner@gmail.com>
2021-10-16 20:15:32 -0600

    Complete librarian tests

--- src/Library.js
+++ src/Library.js
@@ -20,13 +20,14 @@ function addBook(library, book) {
 }
 
 function checkoutBook(library, bookName, genre) {
+  var bookLocation = library.shelves.fantasy;
   if (genre == "fantasy"){
-    for (var i = 0; i < library.shelves.fantasy.length; i++){
-      if (library.shelves.fantasy[i].title.includes(bookName))
+    for (var i = 0; i < bookLocation.length; i++){
+      if (bookLocation[i].title.includes(bookName))
       {
-        library.shelves.fantasy.splice(i, 1);
+        bookLocation.splice(i, 1);
         return `You have now checked out ${bookName} from the ${library.name}`
-      } 
+      }
     }
   }  else if (genre == "fiction"){
       for (var i = 0; i < library.shelves.fiction.length; i++){
@@ -48,11 +49,6 @@ function checkoutBook(library, bookName, genre) {
             return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}`
 }
 
-
-// for(var i = 0; i < shelf.length; i++){
-//   if (shelf[i].title.includes(name)){
-//     hasBook = true;
-//   }
 module.exports = {
   createLibrary,
   addBook,
--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,46 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  }
+
+  greetPatron(name, morning) {
+    if (morning){
+      return `Good morning, ${name}!`
+    } else {
+    return `Hello, ${name}!`
+    }
+  }
+
+  findBook(book){
+
+      for (var i = 0; i < this.library.shelves.fantasy.length; i++){
+        if (book == this.library.shelves.fantasy[i].title){
+              this.library.shelves.fantasy.splice(i, 1)
+              return `Yes, we have ${book}`
+        }
+      }
+      for (var i = 0; i < this.library.shelves.fiction.length; i++){
+        if (book == this.library.shelves.fiction[i].title){
+          this.library.shelves.fantasy.splice(i, 1)
+          return `Yes, we have ${book}`
+        }
+      }
+      for(var i = 0; i < this.library.shelves.nonFiction.length; i++){
+        if (book == this.library.shelves.nonFiction[i].title){
+          this.library.shelves.fantasy.splice(i, 1)
+          return `Yes, we have ${book}`
+        }
+      }
+    return `Sorry, we do not have ${book}`
+  }
+
+  calculateLateFee(days){
+    return Math.ceil(days * 0.25)
   }
 }
+
+
+
 
 module.exports = Librarian;
