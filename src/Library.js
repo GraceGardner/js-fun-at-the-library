59d9358cee0ffbd38744f8e45332f0160850f7a7
Grace Gardner <graceirisgardner@gmail.com>
2021-10-17 14:40:58 -0600

    Refactor checkoutBook function

--- src/Library.js
+++ src/Library.js
@@ -20,32 +20,12 @@ function addBook(library, book) {
 }
 
 function checkoutBook(library, bookName, genre) {
-  var bookLocation = library.shelves.fantasy;
-  if (genre == "fantasy"){
-    for (var i = 0; i < bookLocation.length; i++){
-      if (bookLocation[i].title.includes(bookName))
-      {
-        bookLocation.splice(i, 1);
-        return `You have now checked out ${bookName} from the ${library.name}`
-      }
+  for (var i = 0; i < library.shelves[genre].length; i++){
+    if(library.shelves[genre][i].title == bookName){
+      library.shelves[genre].splice(i, 1)
+      return `You have now checked out ${bookName} from the ${library.name}`
     }
-  }  else if (genre == "fiction"){
-      for (var i = 0; i < library.shelves.fiction.length; i++){
-        if (library.shelves.fiction[i].title.includes(bookName))
-        {
-          library.shelves.fiction.splice(i, 1);
-          return `You have now checked out ${bookName} from the ${library.name}`;
-        }
-      }
-    }  else if (genre == "nonFiction"){
-        for (var i = 0; i < library.shelves.nonFiction.length; i++){
-          if (library.shelves.nonFiction[i].title.includes(bookName))
-          {
-            library.shelves.nonFiction.splice(i, 1);
-            return `You have now checked out ${bookName} from the ${library.name}`
-          }
-      }
-}
+  }
             return `Sorry, there are currently no copies of ${bookName} available at the ${library.name}`
 }
 
